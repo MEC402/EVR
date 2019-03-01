263aadcfe3cb2b41f7cfa45647db9e678cef7746
Kenny-Miller <47906870+Kenny-Miller@users.noreply.github.com>
2019-03-01 16:36:45 -0700

    Updated scripts. Mosque sounds are now working

--- Assets/Scripts/LocationTrackManager.cs
+++ Assets/Scripts/LocationTrackManager.cs
@@ -3,51 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LocationTrackManager : MonoBehaviour {
-	public GameObject camera;
-	private AudioSource[] tracks;
+	public Camera headset;
 
+    public Vector3 headsetRotation;
+	private AudioSource[] tracks;
+    public AudioSource currentClip;
 	private int mosqueTrack;
+    private int fadeRate = 5;
 	private bool mosqueIsPlaying = false;
-	// Use this for initialization
+    private bool peopleMoving = false;
+
+
 	void Start () {
 		tracks = GetComponents<AudioSource>();
-		mosqueTrack = Random.Range(0,1);
-		//StartCoroutine(fadeTrackIn(tracks[mosqueTrack]));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(mosqueIsPlaying == false){
-		mosqueIsPlaying = true;
-		fadeIn(tracks[mosqueTrack]);
-		}
 
-		if(camera.transform.rotation.y > 50){
-			camera.transform.position += new Vector3(50,50,50);
-		}
+        headsetRotation = headset.transform.rotation.eulerAngles;
+
+        //Mosque
+        if (mosqueIsPlaying == false && (260f < headsetRotation.y && headsetRotation.y < 330f)){
+            
+		    mosqueIsPlaying = true;
+            mosqueTrack = Random.Range(0, 1);
+            currentClip = tracks[mosqueTrack];
+            fadeIn();
+        }
+        if(mosqueIsPlaying == true && (headsetRotation.y < 260 || 330 < headsetRotation.y))
+        {
+            StopAllCoroutines();
+            fadeOut();
+        }
+		
+        //People
+        if(peopleMoving = false && (180 < headsetRotation.y && headsetRotation.y < 230))
+        {
+            
+            currentClip = tracks[2];
+            fadeIn();
+        }
+        if (peopleMoving = true && (180 > headsetRotation.y && headsetRotation.y < 230))
+        {
+            StopAllCoroutines();
+            fadeOut();
+        }
+    }
+
+	private void fadeIn(){
+		StartCoroutine(fadeTrackIn());
 	}
 
-	public void fadeIn(AudioSource currentAudio){
-		StartCoroutine(fadeTrackIn(tracks[mosqueTrack]));
-	}
+    private void fadeOut()
+    {
+        StartCoroutine(fadeTrackOut());
+    }
 
-	private IEnumerator fadeTrackIn (AudioSource currentAudio)
+    private IEnumerator fadeTrackIn ()
 	{
-		currentAudio.volume = 0f;
-		currentAudio.Play ();
-		while(currentAudio.volume < 1){
-			currentAudio.volume += Time.deltaTime / 10;
+		currentClip.volume = 0f;
+        currentClip.Play ();
+		while(currentClip.volume < 1){
+
+            currentClip.volume += Time.deltaTime / fadeRate;
 			yield return null;
 		}
 
 	}
-	private IEnumerator fadeTrackOut(AudioSource currentAudio){
-		while(currentAudio.volume > 0){
-			currentAudio.volume -= Time.deltaTime / 1;
+	private IEnumerator fadeTrackOut(){
+		while(currentClip.volume > 0){
+            currentClip.volume -= Time.deltaTime / fadeRate *2;
 			yield return null;
 		}
-		currentAudio.Stop();
-		mosqueIsPlaying = false;
+        if(currentClip.Equals(tracks[0]) || currentClip.Equals(tracks[1]))
+        {
+            mosqueIsPlaying = false;
+        }
+        else
+        {
+            peopleMoving = false;
+        }
+       	
 	}
 
 }
