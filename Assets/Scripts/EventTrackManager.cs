bf58d531593ca2ef3b594f9b4bea18eb228b9914
ikervazquezlopez <ikervazquezlopez@u.boisestate.edu>
2019-04-26 10:53:53 -0600

    Rotation speed (with smoothing) events added, need to tune it

--- Assets/Scripts/EventTrackManager.cs
+++ Assets/Scripts/EventTrackManager.cs
@@ -8,8 +8,11 @@ public class EventTrackManager : MonoBehaviour {
 
 	public Vector3 headsetRotation;
 	public float rotation_speed;
-	private Vector3 current_headset_rotation;
-	private float fast_rotation = 2.0f;
+	private Queue<float> rotation_speed_queue;
+	private int max_queue_size = 50;
+	private Vector3 prev_headset_rotation;
+	private float slow_rotation = 0.4f;
+	private float fast_rotation = 1.1f;
 
 	private AudioSource[] audioManager;
 	private AudioSource current_source;
@@ -37,6 +40,7 @@ public class EventTrackManager : MonoBehaviour {
 
 	private bool alternate = false;
 
+	private float track_overlap =  0.015f;
 	private int fadeRate = 5;
     private int fadeRateMultiplier = 2;
 
@@ -50,8 +54,10 @@ public class EventTrackManager : MonoBehaviour {
 		this.current_source.Play();
 
 		headsetRotation = headset.transform.rotation.eulerAngles;
-		current_headset_rotation = headset.transform.rotation.eulerAngles;
+		this.prev_headset_rotation = headset.transform.rotation.eulerAngles;
 		rotation_speed = 0.0f;
+		this.rotation_speed_queue = new Queue<float>();
+		this.rotation_speed_queue.Enqueue(rotation_speed);
 
 		AutomataReader ar = new AutomataReader("Assets/Scripts/state_machine.txt");
 		this.msm = ar.getStateMachine();
@@ -74,26 +80,39 @@ public class EventTrackManager : MonoBehaviour {
 			rotation_speed = 2.0f;
 		}
 
+		this.headsetRotation = this.headset.transform.rotation.eulerAngles;
+		//this.prev_headset_rotation = headset.transform.rotation.eulerAngles;
+		float curr_rotation_speed = Mathf.Sqrt((headsetRotation.x-this.prev_headset_rotation.x)*(headsetRotation.x-this.prev_headset_rotation.x) +
+				(headsetRotation.y-this.prev_headset_rotation.y)*(headsetRotation.y-this.prev_headset_rotation.y));
+
+
+		if (this.rotation_speed_queue.Count >= this.max_queue_size){
+			this.rotation_speed = this.rotation_speed - this.rotation_speed_queue.Dequeue()/this.max_queue_size;
+		}
+
+		this.rotation_speed_queue.Enqueue(curr_rotation_speed);
+		this.rotation_speed = this.rotation_speed + curr_rotation_speed/this.max_queue_size;
+
+		Debug.Log(this.rotation_speed);
+
+
 		//if (!audioManager[source].isPlaying){
-		if (current_source.time / current_source.clip.length > 0.50){
-		 	current_headset_rotation = headset.transform.rotation.eulerAngles;
-			//rotation_speed = Mathf.Sqrt((headsetRotation.x-current_headset_rotation.x)*(headsetRotation.x-current_headset_rotation.x) +
-//										(headsetRotation.y-current_headset_rotation.y)*(headsetRotation.y-current_headset_rotation.y));
-
-			
-
-
-
+		//if (current_source.time / current_source.clip.length > 0.98){
+		if (this.current_source.time > this.current_source.clip.length - this.track_overlap){ 
 			// Trigger the event
-			if (rotation_speed == 0.0f) {
+			if (rotation_speed <= slow_rotation) {
+				Debug.Log("rest");
 				msm.process_event("rest");
 			}
-			else if (rotation_speed > 0.0f && rotation_speed < fast_rotation) {
+			else if (rotation_speed > slow_rotation && rotation_speed <= fast_rotation) {
+				Debug.Log("slow");
 				msm.process_event("pan_s");
 			}
-			else if (rotation_speed >= fast_rotation) {
+			else if (rotation_speed > fast_rotation) {
+				Debug.Log("fast");
 				msm.process_event("pan_f");
 			}
+
 
 			// Play correct track
 			StateMachine.State current_state = msm.getCurrentState();
@@ -101,8 +120,8 @@ public class EventTrackManager : MonoBehaviour {
 
 			switch(current_state.getName()){
 				case "R0":
-					source = r0;
-					break;
+						source = r0;
+						break;
 				case "R1":
 						source = r1;
 						break;
@@ -138,12 +157,17 @@ public class EventTrackManager : MonoBehaviour {
 						alternate = !alternate;
 						break;
 			}
-			fadeOut(this.current_source);
+			stop_tracks();
+			//fadeOut(this.current_source);
 			this.current_source = this.audioManager[source];
-			fadeIn(this.current_source);
+			this.current_source.Play();
+			//fadeIn(this.current_source);
 			//audioManager[source].Play();
+
+
 		}
-		
+		this.prev_headset_rotation = headset.transform.rotation.eulerAngles;
+
 	}
 
 	private void stop_tracks(){
